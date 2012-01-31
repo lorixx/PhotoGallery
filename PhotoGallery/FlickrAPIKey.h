de47ce067058fdcdd509496d1b359d9504a8892e
lorixx <lorixxhuang@gmail.com>
2012-01-31 00:16:39 -0500

    Start Assignment 4 PhotoGallery kicked off!

---
+++ PhotoGallery/FlickrAPIKey.h
@@ -0,0 +1,18 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Fall 2011.
+//  Copyright 2011 Stanford University. All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"f4e4569d4850869acb9c70b9cbe3cabd"
+
+
+
+//photo gallery
+//Key: f4e4569d4850869acb9c70b9cbe3cabd
+
+//Secret:1f40ddd4e3e44725
