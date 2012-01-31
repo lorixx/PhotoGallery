5b7be64585f8a8ff5eb40550d835c260460ef239
lorixx <lorixxhuang@gmail.com>
2012-01-30 22:21:13 -0500

    Initial Commit

---
+++ PhotoGallery/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  PhotoGallery
+//
+//  Created by Zhisheng Huang on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ PhotoGallery/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  PhotoGallery
+//
+//  Created by Zhisheng Huang on 1/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
